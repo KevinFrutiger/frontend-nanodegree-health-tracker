184071386c49b0144d5f9d6164c75242673e96dc
Kevin Frutiger <git@frutigergroup.com>
2016-02-25 10:33:46 -0800

    test: Revise and add AppView specs
    - revise check for calorie count to pass when zero and nothing on list
    - add check for search list when value submitted via click

--- test/spec/AppViewSpec.js
+++ test/spec/AppViewSpec.js
@@ -1,4 +1,4 @@
-(function() {
+$(function() {
   'use strict';
 
   describe('AppView', function() {
@@ -13,7 +13,7 @@
         }
       );
 
-      expect(modelHasWrongDate).toEqual(false);
+      expect(modelHasWrongDate).toBe(false);
     });
 
     it('renders all saved list items to the page', function() {
@@ -21,20 +21,56 @@
     });
 
     it('renders the calorie total as equal to the sum ' +
-       'of the calories in the list', function() {
+       'of the calories in the list, or 0 if there\'s no list', function() {
       var calorieTotal = Number($('#calorie-total').text());
       var savedCalories = [];
 
       $('.saved-list-item .calories').each(function() {
-        savedCalories.push( Number( $(this).text() ) );
+        savedCalories.push( Number($(this).text()) );
       });
 
-      var savedListItemCaloryTotal = savedCalories.reduce(function(prev, curr) {
-          return prev + curr;
-        });
+      if (savedCalories.length > 0) {
+        var savedListItemCaloryTotal = savedCalories.reduce(function(prev, curr) {
+            return prev + curr;
+          });
+        expect(calorieTotal).toEqual(savedListItemCaloryTotal);
+      } else {
+        expect(calorieTotal).toEqual(0);
+      }
 
-      expect(calorieTotal).toEqual(savedListItemCaloryTotal);
+    });
+
+    var testValue = 'hamburger';
+
+    beforeAll(function(done) {
+      spyOn(app.appView, 'queryHealthAPI').and.callThrough();
+
+      var $input = $('#search-input');
+      var $searchButton = $('#search-btn');
+
+      $input.val(testValue);
+      $searchButton.trigger('click');
+
+      var timeout = setTimeout(function() {
+        done();
+      }, 1000);
+
+    });
+
+    it ('queries API on user click with value', function() {
+
+      expect(app.appView.queryHealthAPI).toHaveBeenCalledWith(testValue);
+
+    });
+
+    it ('show list of results', function(done) {
+
+      expect($('#search-list-container').css('display')).not.toEqual('none');
+
+      expect($('.search-list-item').length).toBeGreaterThan(0);
+
+      done();
     });
   });
 
-})();
+});
